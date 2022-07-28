96cded7cb0ac66536f1904e7b8a33e1065fab845
Maiara Lange <maiaralange@microsoft.com>
2022-07-28 11:48:55 -0300

    Moving sidebar to its own file

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,39 @@
-export function SideBar() {
-  // Complete aqui
+import { Dispatch, SetStateAction } from 'react';
+import { GenreResponseProps } from '../App';
+import { Button } from './Button';
+
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  setSelectedGenreId: Dispatch<SetStateAction<number>>;
+}
+
+export function SideBar({
+  genres,
+  selectedGenreId,
+  setSelectedGenreId
+}: SideBarProps) {
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
